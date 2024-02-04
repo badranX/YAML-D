9bc4cc00a10c141ff6ad39e66a0687cb7904e517
Yahya Badran <techtweaking@gmail.com>
2024-02-04 16:09:35 +0100

    add simple pip setup and README.md

---
+++ setup.py
@@ -0,0 +1,16 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='yaml2d',
+    version='0.0.1',
+    packages=find_packages(),
+    author='Yahya Badran',
+    author_email='techtweaking@gmail.com',
+    description='parser of YAML2D, a tiny subset of YAML',
+    url='https://github.com/badranx/YAML-2D',
+    classifiers=[
+        'License :: MIT License',
+    ],
+)
