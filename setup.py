47146047389074f84c72ea74cbeb085b3ee379dd
Yahya Badran <techtweaking@gmail.com>
2024-02-04 16:34:38 +0100

    fix license and setup

--- setup.py
+++ setup.py
@@ -4,13 +4,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='yaml2d',
-    version='0.0.1',
-    packages=find_packages(),
+    version='0.0.7',
     author='Yahya Badran',
     author_email='techtweaking@gmail.com',
     description='parser of YAML2D, a tiny subset of YAML',
     url='https://github.com/badranx/YAML-2D',
-    classifiers=[
-        'License :: MIT License',
-    ],
+    install_requires=[],
+    packages=find_packages(),
+    license='MIT',
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
