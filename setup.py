1f9360108380f9a6d23d8573032a4eaa5ee8180f
Yahya Badran <techtweaking@gmail.com>
2024-02-10 21:58:53 +0100

    edit README.md

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yamld',
-    version='0.0.11',
+    version='0.0.12',
     author='Yahya Badran',
     author_email='techtweaking@gmail.com',
     description='parser of YAMLd, a tiny subset of YAML',
